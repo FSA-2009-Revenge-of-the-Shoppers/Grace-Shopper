0a9fefd9572fb4b42cd65f6f6c73d0b092f22c99
Josie Chen <josieyxchen@gmail.com>
2020-11-10 15:22:50 -0500

    feat(component): add user from store and render a delete button and the component of creating a product when user.admin is true

--- client/components/all-products.js
+++ client/components/all-products.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import fetchProducts from '../store/products'
+import {fetchProducts, destroyProduct} from '../store/products'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+// edit this line according to David's code
+import ProductForm from './product-form'
 
 class AllProducts extends React.Component {
   componentDidMount() {
@@ -9,19 +11,25 @@ class AllProducts extends React.Component {
   }
 
   render() {
-    const {products} = this.props
+    const {products, user, deleteProduct} = this.props
     return !products.length ? (
       <h1>Loading Products</h1>
     ) : (
       <div>
+        {user.admin && <ProductForm />}
         {products.map(product => {
           return (
             <div key={`product${product.id}`}>
               <img src={product.imageUrl} />
-              <Link to={`/products/${products.id}`}>
+              <Link to={`/products/${product.id}`}>
                 <h2>{product.name}</h2>
               </Link>
               <p>{product.price}</p>
+              {user.admin && (
+                <button type="button" onClick={() => deleteProduct(product.id)}>
+                  Delete
+                </button>
+              )}
             </div>
           )
         })}
@@ -31,11 +39,13 @@ class AllProducts extends React.Component {
 }
 
 const mapState = state => ({
-  products: state.products
+  products: state.products,
+  user: state.user
 })
 
 const mapDispatch = dispatch => ({
-  getProducts: () => dispatch(fetchProducts())
+  getProducts: () => dispatch(fetchProducts()),
+  deleteProduct: productId => dispatch(destroyProduct(productId))
 })
 
 export default connect(mapState, mapDispatch)(AllProducts)
