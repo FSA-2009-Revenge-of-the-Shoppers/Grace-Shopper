12981b5f87eb8007ec255f66dae92ac33f6a0013
Josie Chen <josieyxchen@gmail.com>
2020-11-10 15:19:58 -0500

    feat(redux store): added action types, action functions, thunk functions, and cases for adding and deleting a product

--- client/store/products.js
+++ client/store/products.js
@@ -1,15 +1,27 @@
 import axios from 'axios'
 
-//Action type
+//Action types
 const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
+const ADD_PRODUCT = 'ADD_PRODUCT'
+const DELETE_PRODUCT = 'DELETE_PRODUCT'
 
-//action function
+//action functions
 const getAllProducts = products => ({
   type: GET_ALL_PRODUCTS,
   products
 })
 
-//thunk function
+const addProduct = product => ({
+  type: ADD_PRODUCT,
+  product
+})
+
+const deleteProduct = productId => ({
+  type: DELETE_PRODUCT,
+  productId
+})
+
+//thunk functions
 export const fetchProducts = () => {
   return async dispatch => {
     try {
@@ -21,12 +33,38 @@ export const fetchProducts = () => {
   }
 }
 
+export const postProduct = () => {
+  return async dispatch => {
+    try {
+      const {data: product} = await axios.post('/api/products')
+      dispatch(addProduct(product))
+    } catch (err) {
+      console.log('failed to post a new product', err)
+    }
+  }
+}
+
+export const destroyProduct = productId => {
+  return async dispatch => {
+    try {
+      await axios.delete(`/api/products/${productId}`)
+      dispatch(deleteProduct(productId))
+    } catch (err) {
+      console.log('failed to destroy the product', err)
+    }
+  }
+}
+
 const initialState = []
 
 const productReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_ALL_PRODUCTS:
       return action.products
+    case ADD_PRODUCT:
+      return [...state, action.product]
+    case DELETE_PRODUCT:
+      return state.filter(product => product.id !== action.productId)
     default:
       return state
   }
