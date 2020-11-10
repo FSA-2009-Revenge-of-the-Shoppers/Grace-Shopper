6778e63c1f3aca3f473ec001df83c34bbfd8c21e
Emmanuel Vazquez <emmanuel.vazquez06@gmail.com>
2020-11-10 22:02:55 +0100

    saved associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,8 +2,25 @@ const User = require('./user')
 const Product = require('./product')
 const Cart = require('./cart')
 
-User.belongsToMany(Cart)
-Cart.belongsToMany(User)
+User.hasMany(Cart) //each row in cart table is a different product- (different "cart")
+Cart.belongsTo(User) //** One-To-Many
+
+Cart.belongsTo(Product) //** One-To-Many
+Product.hasMany(Cart)
+
+// has-many magic methods on user. We can use these to load the cart into the store
+// this.accessors = {
+//   get: `get${plural}`,
+//   set: `set${plural}`,
+//   addMultiple: `add${plural}`,
+//   add: `add${singular}`,
+//   create: `create${singular}`,
+//   remove: `remove${singular}`,
+//   removeMultiple: `remove${plural}`,
+//    hasSingle: `has${singular}`,
+//    hasAll: `has${plural}`,
+//    count: `count${plural}`
+//   };
 
 /**
  * We'll export all of our models here, so that any time a module needs a model,
