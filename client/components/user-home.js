e063271b518769b49539309029242b21f8c4dc17
Josie Chen <josieyxchen@gmail.com>
2020-11-10 16:19:42 -0500

    feat(component): imported and rendered All-Product component into User-Home component

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {AllProducts} from './all-products'
+// this part may need edits
 
 /**
  * COMPONENT
