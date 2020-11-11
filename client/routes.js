2f98cd4265d644bc83b657181a70556d94db8016
Josie Chen <josieyxchen@gmail.com>
2020-11-10 22:01:32 -0500

    feat(component): added a component for all users; exported AllUsers component to the index file; added AllUsers react route; render a link to AllUsers component in the navbar when admin logs in

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,14 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, SingleProduct, AllProducts} from './components'
+import {
+  Login,
+  Signup,
+  UserHome,
+  SingleProduct,
+  AllProducts,
+  AllUsers
+} from './components'
 import {me} from './store'
 
 /**
@@ -14,7 +21,7 @@ class Routes extends Component {
   }
 
   render() {
-    const {isLoggedIn} = this.props
+    const {isLoggedIn, isAdmin} = this.props
 
     return (
       <Switch>
@@ -27,6 +34,7 @@ class Routes extends Component {
           <Switch>
             {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
+            {isAdmin && <Route path="/users" component={AllUsers} />}
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
@@ -43,7 +51,9 @@ const mapState = state => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
+    // check if the logged-in user is an admin
+    isAdmin: state.user.admin
   }
 }
 
