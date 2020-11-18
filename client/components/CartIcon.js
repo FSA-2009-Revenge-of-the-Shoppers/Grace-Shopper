3b867d356eefda452b1ba41f198087f293440c86
David Huang <huangdavid42@yahoo.com>
2020-11-18 11:00:46 -0500

    whatever

--- client/components/CartIcon.js
+++ client/components/CartIcon.js
@@ -11,7 +11,7 @@ export class CartIcon extends React.Component {
     )
     return (
       <Link to="/cart" className="icon-container">
-        <img id="icon" src="/shopping-cart.jpg" />
+        <img id="icon" src="/shopping-cart.png" />
         {this.props.cart &&
           this.props.cart.length && <p id="badge">{quantity}</p>}
       </Link>
