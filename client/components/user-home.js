314b07ee55c6edad226742ac4de763b0f83169f4
Danny Berger <danberadi@gmail.com>
2021-01-18 20:45:49 -0500

    Feat(Welcome): Added button to generate new welcome messages on click. Also, formatting

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,44 +1,66 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {AllProducts} from './index'
+import {Button} from '@material-ui/core'
 
-export const UserHome = props => {
-  const {email, isLoggedIn} = props
-
-  const welcomeNum = Math.floor(Math.random() * (8 - 1) + 1)
-
-  let welcomeMsg = ''
-
-  switch (welcomeNum) {
-    case 1:
-      welcomeMsg = `This is the way, ${email}.`
-      break
-    case 2:
-      welcomeMsg = `May the Force be with you, ${email}.`
-      break
-    case 3:
-      welcomeMsg = `Shop or shop not, ${email}. There is no browse.`
-      break
-    case 4:
-      welcomeMsg = `Buy something, ${email}. I have spoken.`
-      break
-    case 5:
-      welcomeMsg = `${email}, I find your lack of spending disturbing.`
-      break
-    case 6:
-      welcomeMsg = `These ARE the products you're looking for, ${email}.`
-      break
-    case 7:
-      welcomeMsg = `${email}, I am YoDaddy.`
-      break
-    default:
-      welcomeMsg = `Welcome, ${email}.`
+export const UserHome = ({email, isLoggedIn}) => {
+  const generateWelcome = () => {
+    let welcomeMsg = ''
+    let welcomeNum = Math.floor(Math.random() * (8 - 1) + 1)
+    switch (welcomeNum) {
+      case 1:
+        welcomeMsg = `This is the way to huge savings, ${email}.`
+        break
+      case 2:
+        welcomeMsg = `May the Force be with your wallet, ${email}.`
+        break
+      case 3:
+        welcomeMsg = `Shop or shop not, ${email}. There is no browse.`
+        break
+      case 4:
+        welcomeMsg = `Buy something, ${email}. I have spoken.`
+        break
+      case 5:
+        welcomeMsg = `${email}, I find your lack of spending disturbing.`
+        break
+      case 6:
+        welcomeMsg = `These ARE the products you're looking for, ${email}.`
+        break
+      case 7:
+        welcomeMsg = `${email}, I am YoDaddy.`
+        break
+      default:
+        welcomeMsg = `Welcome, ${email}.`
+    }
+    return welcomeMsg
   }
+  // Keep track of welcome message in state
+  const [welcome, changeWelcome] = useState('')
+  // When email loads, set welcome message state
+  useEffect(
+    () => {
+      changeWelcome(generateWelcome())
+    },
+    [email]
+  )
 
   return (
-    <div>
-      {isLoggedIn && <h2 id="welcome-msg">{welcomeMsg}</h2>}
+    <div id="user-home-container">
+      {isLoggedIn &&
+        email && (
+          <div id="welcome">
+            <h3 id="welcome-msg">{welcome}</h3>
+            <Button
+              onClick={() => changeWelcome(generateWelcome())}
+              variant="contained"
+              color="primary"
+              id="generate-welcome"
+            >
+              Refresh
+            </Button>
+          </div>
+        )}
       <AllProducts />
     </div>
   )
